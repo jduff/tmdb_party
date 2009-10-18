33e9ca6e4b77c747250c10322a1a4b6564acad96
Jon Maddox <jon@jonsthoughtsoneverything.com>
2009-10-19 05:03:24 +0800

    imdb lookups

--- test/tmdb_party/test_tmdb_party.rb
+++ test/tmdb_party/test_tmdb_party.rb
@@ -53,41 +53,41 @@ class TestTmdbParty < Test::Unit::TestCase
     assert_equal 1, results.length
     assert_equal 'tt0408236', sweeney_todd.imdb_id
   end
-  # 
-  # 
-  # 
-  # test "finding transformers via imdb id" do
-  #   stub_get('/Movie.imdbLookup?api_key=key&imdb_id=tt0418279', 'imdb_search.xml')
-  #   stub_get('/Movie.getInfo?api_key=key&id=1858', 'transformers.xml')
-  #   
-  #   result = @tmdb.imdb_lookup('tt0418279')
-  # 
-  #   # check that the attributes are populated
-  #   assert_equal 52.0, result.popularity
-  #   assert_equal 1.0, result.score
-  #   assert_equal 1858, result.id
-  #   assert_equal 'tt0418279', result.imdb
-  #   assert_equal Date.new(2007, 7, 4), result.release
-  #   assert result.poster.first.is_a?(TMDBParty::Image)
-  #   assert result.backdrop.first.is_a?(TMDBParty::Image)
-  # 
-  #   # how about some that are loaded lazily
-  #   assert_equal "http://www.transformersmovie.com/", result.homepage
-  #   assert_equal "http://www.youtube.com/watch?v=eduwcuq1Exg", result.trailer.url
-  #   
-  #   assert_equal 9, result.categories.length
-  #   assert_equal 36, result.people.length
-  #   
-  #   category = result.categories.detect{|cat| cat.name == "Adventure Film"}
-  #   assert_equal "http://www.themoviedb.org/encyclopedia/category/12", category.url
-  # end
-  # 
-  # test "NOT finding transformers via imdb id" do
-  #   stub_get('/Movie.imdbLookup?api_key=key&imdb_id=tt0418279dd', 'imdb_no_results.xml')
-  #   result = @tmdb.imdb_lookup('tt0418279dd')
-  #   assert_nil result
-  # end
-  # 
+
+  test "finding transformers via imdb id" do
+    stub_get('/Movie.imdbLookup/en/json/key/tt0418279', 'imdb_search.json')
+    stub_get('/Movie.getInfo/en/json/key/1858', 'transformers.json')
+    
+    
+    result = @tmdb.imdb_lookup('tt0418279')
+  
+    # check that the attributes are populated
+    assert_equal 52.0, result.popularity
+    assert_equal 1858, result.id
+    assert_equal 'tt0418279', result.imdb_id
+    assert_equal Date.new(2007, 7, 4), result.released
+    # assert result.poster.first.is_a?(TMDBParty::Image)
+    # assert result.backdrop.first.is_a?(TMDBParty::Image)
+    #   
+    # how about some that are loaded lazily
+    assert_equal "http://www.transformersmovie.com/", result.homepage
+    assert_equal "http://www.youtube.com/watch?v=c0PXr8GV2_Q", result.trailer
+    
+    assert_equal 3, result.genres.length
+    
+    genre = result.genres.detect{|g| g.name == "Adventure"}
+    assert_equal "http://themoviedb.org/encyclopedia/category/12", genre.url
+    
+    assert_equal 132, result.runtime
+    assert_equal 36, result.cast.length
+  end
+
+  test "NOT finding transformers via imdb id" do
+    stub_get('/Movie.imdbLookup/en/json/key/tt0418279dd', 'imdb_no_results.json')
+    result = @tmdb.imdb_lookup('tt0418279dd')
+    assert_nil result
+  end
+  
   # test "no people found" do
   #   stub_get('/Movie.search?api_key=key&title=rad', 'rad.xml')
   #   stub_get('/Movie.getInfo?api_key=key&id=13841', 'no_groups.xml')
