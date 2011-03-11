87e6d5ee58fe36255cb2f1ab7dc3fce06d97b5f6
jduff <duff.john@gmail.com>
2011-03-11 07:57:20 -0500

    refactor method_url so that it's easier to add to the url being generated

--- lib/tmdb_party.rb
+++ lib/tmdb_party.rb
@@ -19,7 +19,7 @@ module TMDBParty
     end
     
     def search(query, lang = @default_lang)
-      data = self.class.get(method_url('Movie.search', query, lang)).parsed_response
+      data = self.class.get(method_url('Movie.search', lang, query)).parsed_response
       if data.class != Array || data.first == "Nothing found."
         []
       else
@@ -28,7 +28,7 @@ module TMDBParty
     end
     
     def search_person(query, lang = @default_lang)
-      data = self.class.get(method_url('Person.search', query, lang)).parsed_response
+      data = self.class.get(method_url('Person.search', lang, query)).parsed_response
       if data.class != Array || data.first == "Nothing found."
         []
       else
@@ -37,7 +37,7 @@ module TMDBParty
     end
     
     def imdb_lookup(imdb_id, lang = @default_lang)
-      data = self.class.get(method_url('Movie.imdbLookup', imdb_id, lang)).parsed_response
+      data = self.class.get(method_url('Movie.imdbLookup', lang, imdb_id)).parsed_response
       if data.class != Array || data.first == "Nothing found."
         nil
       else
@@ -46,17 +46,17 @@ module TMDBParty
     end
     
     def get_info(id, lang = @default_lang)
-      data = self.class.get(method_url('Movie.getInfo', id, lang)).parsed_response
+      data = self.class.get(method_url('Movie.getInfo', lang, id)).parsed_response
       Movie.new(data.first, self)
     end
     
     def get_person(id, lang = @default_lang)
-      data = self.class.get(method_url('Person.getInfo', id, lang)).parsed_response
+      data = self.class.get(method_url('Person.getInfo', lang, id)).parsed_response
       Person.new(data.first, self)
     end
     
     def get_genres(lang = @default_lang)
-      data = self.class.get(method_url('Genres.getList', nil, lang)).parsed_response
+      data = self.class.get(method_url('Genres.getList', lang)).parsed_response
       data[1..-1].collect { |genre| Genre.new(genre) } # Skips the first, see spec/fixtures/genres_results.json
     end
     
@@ -65,9 +65,9 @@ module TMDBParty
         ['json', @api_key]
       end
       
-      def method_url(method, value, lang)
+      def method_url(method, lang, *args)
         url = [method, lang, default_path_items]
-        url << URI.escape(value.to_s) if value
+        url += args.collect{ |a| URI.escape(a.to_s) }
         '/' + url.join('/')
       end
   end
